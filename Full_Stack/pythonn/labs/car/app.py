7b81f39e8e7e18ba170b808da898ecb37650af9f
Ian <ian@Eggs-MacBook-Pro.local>
2018-06-27 13:31:00 -0700

    init commit

--- Full_Stack/pythonn/labs/car/app.py
+++ Full_Stack/pythonn/labs/car/app.py
@@ -6,7 +6,8 @@ from car import Car
 if __name__ == '__main__':
     car1 = Car('Red', 2)
     car2 = Car('Blue', 1)
+    car3 = Car('Yellow', 5)
 
     print(car1)
     print(car2)
-    print([car1, car2])
+    print(car3)
