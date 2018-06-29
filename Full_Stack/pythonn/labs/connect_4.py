45d8782ae182c85079635349ab45c4a891df575d
Ian <ian@Eggs-MacBook-Pro.local>
2018-06-29 10:18:44 -0700

    fully functional

---
+++ Full_Stack/pythonn/labs/connect_4.py
@@ -0,0 +1,115 @@
+# connect_4.py 6/28/18
+
+board_height = 6
+board_width = 7
+player = ['☻', '☺']
+
+def read_moves(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        moves_list = []
+        for move in f.readlines():
+            moves_list.append(int(move.rstrip()) - 1)
+
+        return moves_list
+
+def init_board(board):
+    for i in range(board_height):
+        board.append([]) # create a row
+        for j in range(board_width):
+            board[i].append('_') # fill in the row with -'s
+    return board
+
+def print_board(board):
+    print()
+    print('1 2 3 4 5 6 7', end=' ')
+    for i in range(board_height):
+        print()
+        for j in range(board_width):
+            print(board[i][j], end=' ')
+    print()
+    print()
+
+def make_move(board, move, player):
+    for i in range(board_height):
+        for j in range(board_width):
+            if board[board_height - i - 1][move] == '_':
+                board[board_height - i - 1][move] = player
+                return board
+
+def check_vertical_win(board, player):
+    tmp = []
+    for j in range(board_width):
+        for i in range(board_height):
+            tmp.append(board[board_height - i - 1][j])
+        if (player * 4) in ''.join(tmp):
+            return True
+        else:
+            tmp = []
+    return False
+
+def check_horizontal_win(board, player):
+    tmp = []
+    for i in range(board_height):
+        for j in range(board_width):
+            tmp.append(board[board_height - i - 1][j])
+        if (player * 4) in ''.join(tmp):
+            return True
+        else:
+            tmp = []
+    return False
+
+def check_diagonal_win(board, player):
+    # check bot left to up right diagonal '/'
+    tmp1 = []
+    for i in range(board_height - 3):
+        for j in range(board_width - 3):
+            tmp1.append(board[board_height - i - 1][j])
+            tmp1.append(board[board_height - i - 2][j + 1])
+            tmp1.append(board[board_height - i - 3][j + 2])
+            tmp1.append(board[board_height - i - 4][j + 3])
+            if (player * 4) in ''.join(tmp1):
+                return True
+            else:
+                tmp1 = []
+
+    # check top left to bot right diagonal '\'
+    tmp2 = []
+    for i in range(board_height - 3):
+        for j in range(board_width - 3):
+            tmp2.append(board[i][j])
+            tmp2.append(board[i + 1][j + 1])
+            tmp2.append(board[i + 2][j + 2])
+            tmp2.append(board[i + 3][j + 3])
+            if (player * 4) in ''.join(tmp2):
+                return True
+            else:
+                tmp2 = []
+    return False
+
+def check_win(board, player):
+    if check_vertical_win(board, player) \
+    or check_horizontal_win(board, player) \
+    or check_diagonal_win(board, player):
+        return True
+    return False
+
+def main():
+    board = []
+    board = init_board(board)
+    player_turn = 0
+    while True:
+        move = input("What column would '{}' like to place a piece?: ".format(player[player_turn]))
+        if move.isdigit():
+            if int(move) - 1 in range(7):
+                board = make_move(board, int(move) - 1, player[player_turn])
+                print_board(board)
+                if check_win(board, player[player_turn]):
+                    print("Congrats, {}. You won!!!".format(player[player_turn]))
+                    exit(0)
+                player_turn = 1 - player_turn
+            else:
+                print("That was not a valid column number.")
+        else:
+            print("That was not a number.")
+
+main()
