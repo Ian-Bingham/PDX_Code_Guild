f59d16ca3d3cc169928c98965b9156800f9f19c6
Ian <ianbingham24@gmail.com>
2018-07-09 11:48:39 -0700

    added __len__ and __getitem__

--- Full_Stack/pythonn/labs/blackjack/deck.py
+++ Full_Stack/pythonn/labs/blackjack/deck.py
@@ -14,14 +14,21 @@ class Deck(object):
                 deck.append(Card(rank[i], suit[j]))
         self.deck = deck
 
-    def shuffle(self):
-        return shuffle(self.deck)
-
     def __repr__(self):
         temp = ''
         for i in range(len(self.deck)):
             temp += '{} of {}\n'.format(self.deck[i].rank, self.deck[i].suit)
         return temp
+
+    def __len__(self):
+        return len(self.deck)
+
+    def __getitem__(self, position):
+        return self.deck[position]
+        # deck[5]
+
+    def shuffle(self):
+        return shuffle(self.deck)
 
     def cut_deck(self):
         cut = choice(range(10, 52))
@@ -32,13 +39,21 @@ class Deck(object):
 
 if __name__ == '__main__':
     deck = Deck()
+    print("Deck")
     print(deck)
+
     print('*' * 80)
     deck.shuffle()
+    print("Shuffled Deck")
     print(deck)
+
     print('*' * 80)
     deck.cut_deck()
+    print("Cut Deck")
     print(deck)
+
     print('*' * 80)
+    print("Length of Deck:", str(len(deck)))
+    print("7th Card in Deck:", deck[6])
     top_card = deck.draw_card()
-    print(top_card)
+    print("Draw Card:", top_card)
