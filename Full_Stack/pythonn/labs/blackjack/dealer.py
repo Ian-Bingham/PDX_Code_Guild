e47a2da8ba5565affc78e74b894e8ac36f75ce96
Ian <ianbingham24@gmail.com>
2018-07-10 16:30:56 -0700

    created Dealer class to show one card and hide the other

---
+++ Full_Stack/pythonn/labs/blackjack/dealer.py
@@ -0,0 +1,15 @@
+# dealer.py 7/10/18
+
+from hand import Hand
+
+class Dealer(Hand):
+    def __init__(self):
+        super().__init__()
+
+    def visible_card(self):
+        return "{} of {}".format(self.hand[1].rank, self.hand[1].suit)
+
+    def visible_score(self):
+        score_conversion = {str(key):key for key in range(2, 11)}
+        score_conversion.update({'A': 1, 'J': 10, 'Q': 10, 'K': 10})
+        return "{}".format(score_conversion[self.hand[1].rank])
