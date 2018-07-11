1ed82af808906b7c5dc068203832261463674e6e
Ian <ianbingham24@gmail.com>
2018-07-11 12:19:11 -0700

    added comments

--- Full_Stack/pythonn/labs/atm/app.py
+++ Full_Stack/pythonn/labs/atm/app.py
@@ -2,42 +2,24 @@
 
 from atm import ATM
 
-# atm = ATM()
-# print(atm.check_balance())
-# print(atm.deposit(1000))
-# print(atm.withdraw(500))
-# print(atm.deposit(1))
-# print(atm.withdraw(100))
-# print(atm.calc_interest())
-# atm.print_transactions()
-#
-# print('*' * 80)
-#
-# atm2 = ATM(5000, 0.065)
-# print(atm2.check_balance())
-# print(atm2.deposit(1000))
-# print(atm2.withdraw(700))
-# print(atm2.deposit(2000))
-# print(atm2.withdraw(5000))
-# print(atm2.withdraw(3000))
-# print(atm2.calc_interest())
-# atm2.print_transactions()
+def main():
+    atm3 = ATM() # create ATM object
+    while True:
+        user_option = input("Choose an option: (D)eposit, (W)ithdraw, (C)heck Balance, (H)istory, \
+    (hit return if finished): ") # get command from user
 
-atm3 = ATM()
-while True:
-    user_option = input("Choose an option: (D)eposit, (W)ithdraw, (C)heck Balance, (H)istory, \
-(hit return if finished): ")
+        if not user_option: # check if the user is done using the atm
+            print("Goodbye.")
+            break
+        if user_option.lower() in ['d', 'deposit']: # deposit amount
+            amount = input("How much would you like to deposit?: ")
+            print(atm3.deposit(int(amount)))
+        if user_option.lower() in ['w', 'withdraw']: # withdraw amount
+            amount = input("How much would you like to withdraw?: ")
+            print(atm3.withdraw(int(amount)))
+        if user_option.lower() in ['c', 'check balance']: # show their balance
+            print(atm3.check_balance())
+        if user_option.lower() in ['h', 'history']: # show a history of transactions
+            atm3.print_transactions()
 
-    if not user_option:
-        print("Goodbye.")
-        break
-    if user_option.lower() in ['d', 'deposit']:
-        amount = input("How much would you like to deposit?: ")
-        print(atm3.deposit(int(amount)))
-    if user_option.lower() in ['w', 'withdraw']:
-        amount = input("How much would you like to withdraw?: ")
-        print(atm3.withdraw(int(amount)))
-    if user_option.lower() in ['c', 'check balance']:
-        print(atm3.check_balance())
-    if user_option.lower() in ['h', 'history']:
-        atm3.print_transactions()
+main()
--- Full_Stack/pythonn/labs/atm/atm.py
+++ Full_Stack/pythonn/labs/atm/atm.py
@@ -6,19 +6,26 @@ class ATM(object):
         self.balance = balance
         self.interest_rate = interest_rate
 
+    # return the balance
     def check_balance(self):
         return 'Your balance is ${}.'.format(self.balance)
 
+    # add the deposited amount to the balance
+    # update transaction history
     def deposit(self, amount):
         self.balance += amount
         self.transaction_list.append('User deposited ${}'.format(amount))
         return 'Deposit complete.'
 
+    # return True if they have enough funds to perform the withdrawal
     def check_withdrawal(self, amount):
         if self.balance - amount >= 0:
             return True
         return False
 
+    # if they have enough funds, subtract the amount from their balance
+    # if not, print an error message
+    # update transaction history
     def withdraw(self, amount):
         if self.check_withdrawal(amount):
             self.balance -= amount
@@ -26,9 +33,11 @@ class ATM(object):
             return 'Withdrawal complete.'
         return 'Not enough funds to withdraw ${} from your account.'.format(amount)
 
+    # calculate their interest
     def calc_interest(self):
         return 'Your interest is ${}'.format(self.balance * self.interest_rate)
 
+    # print the list of transactions
     def print_transactions(self):
         print('----- Transaction History -----')
         for i in self.transaction_list:
