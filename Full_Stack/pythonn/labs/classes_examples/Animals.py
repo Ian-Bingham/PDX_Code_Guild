479611ff2a4586223b339bbbd5cef3080bec9fff
Ian <ianbingham24@gmail.com>
2018-07-12 14:23:16 -0700

    commented code, pep8

--- Full_Stack/pythonn/labs/classes_examples/Animals.py
+++ Full_Stack/pythonn/labs/classes_examples/Animals.py
@@ -4,86 +4,90 @@ Animal.py
 Extend the Animal class with two animal subclasses.
 """
 
+
 class Animal:
-	def __init__(self, species, name):
-		self.species = species
-		self.name = name
+    def __init__(self, species, name):
+        self.species = species
+        self.name = name
 
-	def move(self):
-		print(f'{self.name} moves.')
+    def move(self):
+        print(f'{self.name} moves.')
 
-	def __repr__(self):
-		return f'{self.name} is a {self.species}.'
+    def __repr__(self):
+        return f'{self.name} is a {self.species}.'
 
 
+# Bird inherits from Animal
 class Bird(Animal):
-	def __init__(self, name):
-		super().__init__('bird', name)
+    # get all of the same methods in Animal
+    def __init__(self, name):
+        super().__init__('bird', name)
 
-	def move(self):
-		print(f'{self.name} flaps.')
+    # override move method from Animal class
+    def move(self):
+        print(f'{self.name} flaps.')
 
 
 class Cat(Animal):
-	def __init__(self, name):
-		super().__init__('cat', name)
+    # get all of the same methods in Animal
+    def __init__(self, name):
+        super().__init__('cat', name)
 
-	def move(self):
-		print(f'{self.name} walks.')
+    # override move method
+    def move(self):
+        print(f'{self.name} walks.')
 
-	def talk(self):
-		print(f'{self.name} says meow!')
+    # create a new method called talk()
+    def talk(self):
+        print(f'{self.name} says meow!')
 
 
 class Eagle(Bird):
-	def __init__(self, name):
-		super().__init__(name)
+    # get all of the same methods in Bird
+    def __init__(self, name):
+        super().__init__(name)
 
-	def move(self):
-		super().move()
+    # create a new method called merica()
+    def merica(self):
+        print(f"{self.name} flys around carrying a US flag and bacon.")
 
-	def merica(self):
-		print(f"{self.name} flys around carrying a US flag and bacon.")
 
 class Tiger(Cat):
-	def __init__(self, name):
-		super().__init__(name)
+    # get all of the same methods in Cat
+    def __init__(self, name):
+        super().__init__(name)
 
-	def move(self):
-		super().move()
-		print(f"{self.name} also stalks its prey")
-
-	def talk(self):
-		super().talk()
-
-
+    # inherit the move method, but add more to it
+    def move(self):
+        super().move()
+        print(f"{self.name} also stalks its prey")
 
 if __name__ == '__main__':
-	print("*" * 20)
-	arbitrary_animal = Animal('jackalope', 'lucy')
-	print(arbitrary_animal)
-	arbitrary_animal.move()
+    print("*" * 20)
+    arbitrary_animal = Animal('jackalope', 'lucy')
+    print(arbitrary_animal)
+    arbitrary_animal.move()
 
-	print("*" * 20)
-	bird = Bird('screaming boy')
-	print(bird)
-	bird.move()
+    print("*" * 20)
+    bird = Bird('screaming boy')
+    print(bird)
+    bird.move()
 
-	# Test your animal here
-	print("*" * 20)
-	cat = Cat("Sammy")
-	print(cat)
-	cat.move()
-	cat.talk()
+    # Test your animal here
+    print("*" * 20)
+    cat = Cat("Sammy")
+    print(cat)
+    cat.move()
+    cat.talk()
 
-	print("*" * 20)
-	eagle = Eagle("Caw Caw")
-	print(eagle)
-	eagle.move()
-	eagle.merica()
+    print("*" * 20)
+    eagle = Eagle("Caw Caw")
+    print(eagle)
+    eagle.move()
+    eagle.merica()
 
-	print("*" * 20)
-	tiger = Tiger("Tony")
-	print(tiger)
-	tiger.move()
-	tiger.talk()
+    print("*" * 20)
+    tiger = Tiger("Tony")
+    print(tiger)
+    tiger.move()
+    tiger.talk()
--- Full_Stack/pythonn/labs/classes_examples/CodeGuild.py
+++ Full_Stack/pythonn/labs/classes_examples/CodeGuild.py
@@ -3,49 +3,54 @@ CodeGuild.py
 Define CodeGuild to extend the school class
 """
 
+
 class School:
-	def __init__(self, name, **kwargs):
-		self.name = name
-		self.instructors = kwargs.pop('instructors', [])
-		self.courses = kwargs.pop('courses', [])
-		self.students = kwargs.pop('students', [])
+    def __init__(self, name, **kwargs):
+        self.name = name
 
-	def add_instructor(self, instructor):
-		self.instructors.append(instructor)
+        # if the keyword exists in kwargs grab that list
+        self.instructors = kwargs.pop('instructors', [])
+        self.courses = kwargs.pop('courses', [])
+        self.students = kwargs.pop('students', [])
 
-	def add_course(self, course):
-		self.courses.append(course)
+    def add_instructor(self, instructor):
+        self.instructors.append(instructor)
 
-	def add_student(self, student):
-		self.students.append(student)
+    def add_course(self, course):
+        self.courses.append(course)
+
+    def add_student(self, student):
+        self.students.append(student)
 
 
-# Define this class
+# inherit all methods from School
 class CodeGuild(School):
-	def __init__(self, **kwargs):
-		super().__init__('CodeGuild', **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__('CodeGuild', **kwargs)
 
 
 if __name__ == '__main__':
-	kwargs1 = {"instructors": ['Bob', 'Joe', 'Kyle'], "courses": ['math', 'science', 'history'], "students": ['George']}
-	arbitrary_school = School('Joy of Painting', **kwargs1)
-	arbitrary_school.add_course('Intro to Life and Living')
-	arbitrary_school.add_course('Being Kind')
-	arbitrary_school.add_course('Bush Rendering')
-	arbitrary_school.add_instructor("Bob Ross' Spirit")
-	arbitrary_school.add_student('me')
-	arbitrary_school.add_student('you')
-	print(f"Name: {arbitrary_school.name}")
-	print(f"Courses: {arbitrary_school.courses}")
-	print(f"Instructors: {arbitrary_school.instructors}")
-	print(f"Students: {arbitrary_school.students}")
+    kwargs1 = {"instructors": ['Bob', 'Joe', 'Kyle'],
+               "courses": ['math', 'science', 'history'],
+               "students": ['George']}
+    arbitrary_school = School('Joy of Painting', **kwargs1)
+    arbitrary_school.add_course('Intro to Life and Living')
+    arbitrary_school.add_course('Being Kind')
+    arbitrary_school.add_course('Bush Rendering')
+    arbitrary_school.add_instructor("Bob Ross' Spirit")
+    arbitrary_school.add_student('me')
+    arbitrary_school.add_student('you')
+    print(f"Name: {arbitrary_school.name}")
+    print(f"Courses: {arbitrary_school.courses}")
+    print(f"Instructors: {arbitrary_school.instructors}")
+    print(f"Students: {arbitrary_school.students}")
 
-
-	# Test your CodeGuild class here
-	print("*" * 80)
-	kwargs2 = {"instructors": ['a', 'b', 'c'], "courses": ['breakfast', 'lunch', 'dinner']}
-	codeguild = CodeGuild(**kwargs2)
-	print(f"Name: {codeguild.name}")
-	print(f"Courses: {codeguild.courses}")
-	print(f"Instructors: {codeguild.instructors}")
-	print(f"Students: {codeguild.students}")
+    # Test your CodeGuild class here
+    print("*" * 80)
+    kwargs2 = {"instructors": ['a', 'b', 'c'],
+               "courses": ['breakfast', 'lunch', 'dinner']}
+    codeguild = CodeGuild(**kwargs2)
+    print(f"Name: {codeguild.name}")
+    print(f"Courses: {codeguild.courses}")
+    print(f"Instructors: {codeguild.instructors}")
+    print(f"Students: {codeguild.students}")
