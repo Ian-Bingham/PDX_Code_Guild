c3daff67b9f6bb0ae5426a378ba83181403f0254
Ian <ianbingham24@gmail.com>
2018-08-02 17:19:44 -0700

    simple countdown working, stop button not working

--- Full_Stack/javascript/GUI/clock/clock.js
+++ Full_Stack/javascript/GUI/clock/clock.js
@@ -59,6 +59,72 @@ function lapTimer() {
     lapnum += 1;
 }
 
+function convertHours(deadline) {
+    let time = new Date();
+    time.setHours(deadline, 0, 0, 0);
+    return time;
+}
+
+function convertMinutes(deadline) {
+    let time = new Date();
+    let hours = Math.floor(deadline / 60);
+    let minutes = deadline % 60;
+    time.setHours(hours, minutes, 0, 0);
+    return time;
+}
+
+function convertSeconds(deadline) {
+    let time = new Date();
+    let hours = Math.floor(deadline / (60 * 60));
+    let minutes = Math.floor(deadline / 60) % 60;
+    let seconds = deadline % 60;
+    time.setHours(hours, minutes, seconds, 0);
+    return time;
+}
+
+function convertSimpleDeadline() {
+    let deadline = $('#timeInput').val();
+    let time = null;
+    if($('#cdUnit').val() == 'hoursUnit'){
+        time = convertHours(deadline);
+    }
+    else if($('#cdUnit').val() == 'minutesUnit'){
+        time = convertMinutes(deadline);
+    }
+    else if($('#cdUnit').val() == 'secondsUnit'){
+        time = convertSeconds(deadline);
+    }
+
+    simpleDeadlineInt = setInterval(function() {
+        if(time.getHours() == 0 &&
+            time.getMinutes() == 0 &&
+            time.getSeconds() == 0) {
+            clearInterval(simpleDeadlineInt);
+            alert('DONE!!!!!')
+        }
+        time.setSeconds(time.getSeconds() - 1);
+        $('#cdSimpleTime').text(`${formatTime(time.getHours())}:${formatTime(time.getMinutes())}:${formatTime(time.getSeconds())}`)
+    }, 1000);
+}
+
+// function getDateDeadline() {
+//     let deadline = $('#dateInput').val();
+//     let month = deadline.slice(0, 2) - 1; //month index from 0-11
+//     let day = deadline.slice(3, 5);
+//     let year = deadline.slice(6);
+//     return [month, day, year];
+// }
+//
+// function calcMsRemaining() {
+//     let now = new Date();
+//     msRemaining = deadline - now;
+//     if(msRemaining <= 0){
+//         clearInterval(countDownInt);
+//         $('#countdownDateDone').show();
+//     }
+//     $('#countdownDateTime').text(msRemaining)
+// }
+
 $('#startBtn').on('click', function() {
     timerInt = setInterval(startTimer, 10);
     $('#startBtn').prop('disabled', true);
@@ -86,8 +152,24 @@ $('#lapBtn').on('click', function() {
     lapTimer();
 });
 
+$('#cdSimpleStartBtn').on('click', function() {
+    convertSimpleDeadline();
+});
+
+// $('#cdSimpleStopBtn').on('click', function() {
+//     clearInterval(simpleDeadlineInt);
+// });
+
+// $('#cdDateBtn').on('click', function() {
+//     let [m, d, y] = getDateDeadline();
+//     deadline = new Date(y, m, d);
+//     countdownInt = setInterval(calcMsRemaining, 1000);
+// });
+
 let clockInt = setInterval(clock, 1000);
 
 let time = new Date();
 time.setHours(0, 0, 0, 0);
 let lapnum = 1;
+
+// $('#cdDateDone').hide();
