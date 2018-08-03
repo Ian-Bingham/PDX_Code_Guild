7dbf4bec898136945144ad9aa4f45fd3708eba93
Ian <ianbingham24@gmail.com>
2018-08-03 11:18:31 -0700

    fixed simple stop btn, added simple reset btn

--- Full_Stack/javascript/GUI/clock/clock.js
+++ Full_Stack/javascript/GUI/clock/clock.js
@@ -21,8 +21,8 @@ function clock() {
     m = formatTime(m);
     s = formatTime(s);
 
-    let clock = document.getElementById('clock');
-    clock.innerText = 'Current Time: ' + h + ':' + m + ':' + s;
+    let clock = $('#clock');
+    clock.text(`Current Time: ${h}:${m}:${s}`);
 }
 
 function startTimer() {
@@ -35,10 +35,7 @@ function startTimer() {
     s = formatTime(s);
     ms = formatTime(ms / 10);
 
-    document.getElementById('timer').innerText = h +
-        ':' + m +
-        ':' + s +
-        ':' + ms;
+    $('#timer').text(`${h}:${m}:${s}.${ms}`)
 }
 
 function stopTimer() {
@@ -49,81 +46,54 @@ function stopTimer() {
 function resetTimer() {
     document.getElementById('timer').style.color = 'black';
     time.setHours(0, 0, 0, 0);
-    document.getElementById('timer').innerText = '00:00:00:00';
+    $('#timer').text('00:00:00:00');
     $('#laplist').empty();
     lapnum = 1;
 }
 
 function lapTimer() {
-    $('#laplist').append(`<li>Lap ${lapnum}: ${h}:${m}:${s}:${ms}</li>`);
+    $('#laplist').append(`<li>Lap ${lapnum}: ${h}:${m}:${s}.${ms}</li>`);
     lapnum += 1;
 }
 
-function convertHours(deadline) {
-    let time = new Date();
-    time.setHours(deadline, 0, 0, 0);
-    return time;
+function convertHours(dlText) {
+    let dlTime = new Date();
+    dlTime.setHours(dlText, 0, 0, 0);
+    return dlTime;
 }
 
-function convertMinutes(deadline) {
-    let time = new Date();
-    let hours = Math.floor(deadline / 60);
-    let minutes = deadline % 60;
-    time.setHours(hours, minutes, 0, 0);
-    return time;
+function convertMinutes(dlText) {
+    let dlTime = new Date();
+    let hours = Math.floor(dlText / 60);
+    let minutes = dlText % 60;
+    dlTime.setHours(hours, minutes, 0, 0);
+    return dlTime;
 }
 
-function convertSeconds(deadline) {
-    let time = new Date();
-    let hours = Math.floor(deadline / (60 * 60));
-    let minutes = Math.floor(deadline / 60) % 60;
-    let seconds = deadline % 60;
-    time.setHours(hours, minutes, seconds, 0);
-    return time;
+function convertSeconds(dlText) {
+    let dlTime = new Date();
+    let hours = Math.floor(dlText / (60 * 60));
+    let minutes = Math.floor(dlText / 60) % 60;
+    let seconds = dlText % 60;
+    dlTime.setHours(hours, minutes, seconds, 0);
+    return dlTime;
 }
 
 function convertSimpleDeadline() {
-    let deadline = $('#timeInput').val();
-    let time = null;
+    let dlText = $('#timeInput').val();
+    let dlTime = null;
     if($('#cdUnit').val() == 'hoursUnit'){
-        time = convertHours(deadline);
+        dlTime = convertHours(dlText);
     }
     else if($('#cdUnit').val() == 'minutesUnit'){
-        time = convertMinutes(deadline);
+        dlTime = convertMinutes(dlText);
     }
     else if($('#cdUnit').val() == 'secondsUnit'){
-        time = convertSeconds(deadline);
+        dlTime = convertSeconds(dlText);
     }
 
-    simpleDeadlineInt = setInterval(function() {
-        if(time.getHours() == 0 &&
-            time.getMinutes() == 0 &&
-            time.getSeconds() == 0) {
-            clearInterval(simpleDeadlineInt);
-            alert('DONE!!!!!')
-        }
-        time.setSeconds(time.getSeconds() - 1);
-        $('#cdSimpleTime').text(`${formatTime(time.getHours())}:${formatTime(time.getMinutes())}:${formatTime(time.getSeconds())}`)
-    }, 1000);
+    return dlTime;
 }
-
-// function getDateDeadline() {
-//     let deadline = $('#dateInput').val();
-//     let month = deadline.slice(0, 2) - 1; //month index from 0-11
-//     let day = deadline.slice(3, 5);
-//     let year = deadline.slice(6);
-//     return [month, day, year];
-// }
-//
-// function calcMsRemaining() {
-//     let now = new Date();
-//     msRemaining = deadline - now;
-//     if(msRemaining <= 0){
-//         clearInterval(countDownInt);
-//         $('#countdownDateDone').show();
-//     }
-//     $('#countdownDateTime').text(msRemaining)
-// }
 
 $('#startBtn').on('click', function() {
     timerInt = setInterval(startTimer, 10);
@@ -153,23 +123,49 @@ $('#lapBtn').on('click', function() {
 });
 
 $('#cdSimpleStartBtn').on('click', function() {
-    convertSimpleDeadline();
+    $('#cdSimpleStartBtn').prop('disabled', true);
+    document.getElementById('cdSimpleTime').style.color = 'black';
+    console.log($('#cdSimpleTime').text())
+    if($('#cdSimpleTime').text() == '00:00:00') {
+        dlTime = convertSimpleDeadline();
+    }
+    else {
+        let currDLTime = $('#cdSimpleTime').text();
+        let currDLHours = currDLTime.slice(0,2);
+        let currDLMinutes = currDLTime.slice(3,5);
+        let currDLSeconds = currDLTime.slice(6);
+        dlTime.setHours(currDLHours, currDLMinutes, currDLSeconds);
+    }
+
+    simpleDeadlineInt = setInterval(function() {
+        let dlH = dlTime.getHours();
+        let dlM = dlTime.getMinutes();
+        let dlS = dlTime.getSeconds();
+        if(dlH == 0 && dlM == 0 && dlS == 0) {
+            clearInterval(simpleDeadlineInt);
+            alert('DONE!!!!!')
+        }
+        dlTime.setSeconds(dlS - 1);
+
+        $('#cdSimpleTime').text(`${formatTime(dlH)}:${formatTime(dlM)}:${formatTime(dlS)}`)
+    }, 1000);
+
 });
 
-// $('#cdSimpleStopBtn').on('click', function() {
-//     clearInterval(simpleDeadlineInt);
-// });
+$('#cdSimpleStopBtn').on('click', function() {
+    $('#cdSimpleStartBtn').prop('disabled', false);
+    document.getElementById('cdSimpleTime').style.color = 'red';
+    clearInterval(simpleDeadlineInt);
+});
 
-// $('#cdDateBtn').on('click', function() {
-//     let [m, d, y] = getDateDeadline();
-//     deadline = new Date(y, m, d);
-//     countdownInt = setInterval(calcMsRemaining, 1000);
-// });
+$('#cdSimpleResetBtn').on('click', function() {
+    document.getElementById('cdSimpleTime').style.color = 'black';
+    $('#cdSimpleTime').text('00:00:00');
+    dlTime.setHours(0, 0, 0, 0)
+});
 
 let clockInt = setInterval(clock, 1000);
 
 let time = new Date();
 time.setHours(0, 0, 0, 0);
 let lapnum = 1;
-
-// $('#cdDateDone').hide();
