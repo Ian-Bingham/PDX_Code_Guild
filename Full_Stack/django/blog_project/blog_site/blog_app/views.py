7287c59ff0b85a3fdfd2daa46a9061cd8cf4ad4a
Ian <ianbingham24@gmail.com>
2018-08-21 15:41:14 -0700

    permissions

--- Full_Stack/django/blog_project/blog_site/blog_app/views.py
+++ Full_Stack/django/blog_project/blog_site/blog_app/views.py
@@ -6,11 +6,14 @@ from .models import BlogPost, Comment
 
 
 # Create your views here.
+@login_required
 def post_list(request):
     blogs_list = BlogPost.objects.all()
     return render(request, 'blog_app/post_list.html', {'blogs_list': blogs_list})
 
 
+@login_required
+@permission_required('blog_app.add_blogpost')
 def make_post(request):
     if request.method == 'POST':
         form = MakePostForm(request.POST)
@@ -26,12 +29,14 @@ def make_post(request):
     return render(request, 'blog_app/make_post.html', {'form': form})
 
 
+@login_required
 def post_detail(request, blog_id):
     blog = get_object_or_404(BlogPost, id=blog_id)
     form = MakeCommentForm()
     return render(request, 'blog_app/post_detail.html', {'form': form, 'blog': blog})
 
 
+@login_required
 def make_comment(request, blog_id):
     blog = get_object_or_404(BlogPost, id=blog_id)
     if request.method == 'POST':
