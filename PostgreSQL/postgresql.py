c1a86048bbd5ad8f993bc3580343b6375c0ae42c
Ian <ianbingham24@gmail.com>
2018-10-01 16:32:49 -0700

    insert multiple items

--- PostgreSQL/postgresql.py
+++ PostgreSQL/postgresql.py
@@ -1,25 +1,51 @@
 import psycopg2
 from psycopg2 import Error
+
 try:
-   connection = psycopg2.connect(user="postgres",
-                                  password="",
-                                  host="127.0.0.1",
-                                  port="5433",
-                                  database="sample_db")
-   postgres_insert_query_one = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (1,'IPhone X','900')"""
-   postgres_insert_query_two = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (2,'Google Pixel','700')"""
-   cursor = connection.cursor()
-   cursor.execute(postgres_insert_query_one)
-   cursor.execute(postgres_insert_query_two)
-   connection.commit()
-   print ("Records inserted successfully into mobile")
-except (Exception, psycopg2.DatabaseError) as error :
+    connection = psycopg2.connect(user = 'ian',
+                                password = '',
+                                host = 'localhost',
+                                port = '5432',
+                                database = 'test_db')
+
+    cursor = connection.cursor()
+
+    create_table_query = """CREATE TABLE groceries
+                            (id INT PRIMARY KEY NOT NULL,
+                            name TEXT NOT NULL,
+                            price REAL);"""
+
+    insert_apple_query = """INSERT INTO groceries (id, name, price) VALUES
+                            (1, 'apple', 0.68)"""
+
+    insert_banana_query = """INSERT INTO groceries (id, name, price) VALUES
+                            (2, 'banana', 0.49)"""
+
+    insert_chicken_query = """INSERT INTO groceries (id, name, price) VALUES
+                            (3, 'chicken', 0.98)"""
+
+    multiple_items_list = [ (4, 'almonds', 4.37),
+                            (5, 'cheese', 1.88),
+                            (6, 'broccoli', 0.77) ]
+    insert_multiple_query = """INSERT INTO groceries (id, name, price) VALUES
+                            (%s, %s, %s) """
+
+    cursor.execute(create_table_query)
+    cursor.execute(insert_apple_query)
+    cursor.execute(insert_banana_query)
+    cursor.execute(insert_chicken_query)
+    cursor.executemany(insert_multiple_query, multiple_items_list)
+    connection.commit()  # save changes to the database
+    print('Inserted multiple items')
+
+except(Exception, psycopg2.DatabaseError) as error:
     if(connection):
-        connection.rollback()
-    print("Failed inserting record into mobile table {}".format(error))
+        connection.rollback()  # rollback all changes to the database since last commit
+    print(f"Failed inserting values: {error}")
+
 finally:
-    #closing database connection.
+    # closing database connection
     if(connection):
         cursor.close()
         connection.close()
-        print("PostgreSQL connection is closed")
+        print("PostgreSQL connection is closed.")
