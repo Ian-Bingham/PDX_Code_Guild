2a539c35840aaf3bed6181007bdff73f9e9a280f
Ian <ianbingham24@gmail.com>
2018-10-01 17:12:00 -0700

    select query and cursor.fetch()

--- PostgreSQL/postgresql.py
+++ PostgreSQL/postgresql.py
@@ -13,7 +13,7 @@ try:
     create_table_query = """CREATE TABLE groceries
                             (id INT PRIMARY KEY NOT NULL,
                             name TEXT NOT NULL,
-                            price REAL);"""
+                            price REAL)"""
 
     insert_apple_query = """INSERT INTO groceries (id, name, price) VALUES
                             (1, 'apple', 0.68)"""
@@ -24,19 +24,27 @@ try:
     insert_chicken_query = """INSERT INTO groceries (id, name, price) VALUES
                             (3, 'chicken', 0.98)"""
 
-    multiple_items_list = [ (4, 'almonds', 4.37),
+    records_to_insert = [ (4, 'almonds', 4.37),
                             (5, 'cheese', 1.88),
                             (6, 'broccoli', 0.77) ]
     insert_multiple_query = """INSERT INTO groceries (id, name, price) VALUES
-                            (%s, %s, %s) """
+                            (%s, %s, %s)"""
 
-    cursor.execute(create_table_query)
-    cursor.execute(insert_apple_query)
-    cursor.execute(insert_banana_query)
-    cursor.execute(insert_chicken_query)
-    cursor.executemany(insert_multiple_query, multiple_items_list)
-    connection.commit()  # save changes to the database
-    print('Inserted multiple items')
+    # cursor.execute(create_table_query)
+    # cursor.execute(insert_apple_query)
+    # cursor.execute(insert_banana_query)
+    # cursor.execute(insert_chicken_query)
+    # cursor.executemany(insert_multiple_query, records_to_insert)
+    # connection.commit()  # save changes to the database
+    # print("Created table 'groceries' and insertd items")
+
+
+    cursor2 = connection.cursor()
+    cursor2.execute("SELECT * FROM groceries")
+    groceries_records = cursor2.fetchmany(2)
+    print("Displaying rows from groceries table using cursor2.fetchmany()")
+    for row in groceries_records:
+        print(row)
 
 except(Exception, psycopg2.DatabaseError) as error:
     if(connection):
